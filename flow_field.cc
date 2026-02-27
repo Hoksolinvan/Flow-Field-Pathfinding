505f52f8d34d8e82a9162ab9366f3ca27af66626
Hoksolinvan <chhunhoksolinvan.prof@gmail.com>
2026-02-27 11:36:31 -0800

    Implemented GenerateFlowField, Print_Cost,Integration,Flow_Field functions

--- flow_field.cc
+++ flow_field.cc
@@ -1,4 +1,5 @@
 #include "flow_field.hpp"
+#include <iostream>
 #include <stdio.h>
 #include <utility>
 #include <stdexcept>
@@ -6,6 +7,8 @@
 #include <limits>
 #include <queue>
 
+
+#define range_of_motion 4
 
 GenerateFlowField::GenerateFlowField(int goal_x, int goal_y, int tile_size_x, int tile_size_y) 
         : goal_x(goal_x), goal_y(goal_y), tile_size_x(tile_size_x), tile_size_y(tile_size_y),
@@ -19,6 +22,7 @@ GenerateFlowField::GenerateFlowField(int goal_x, int goal_y, int tile_size_x, in
 std::vector<std::vector<Tile>>& GenerateFlowField::Generate() {
         Generate_Cost_Field();
         Generate_Integration_Field();
+        Generate_Flow_Field();
 
         std::vector<std::vector<Tile>>* p = nullptr; 
         return p;
@@ -38,7 +42,7 @@ void GenerateFlowField::Generate_Cost_Field(){
 void GenerateFlowField::Generate_Integration_Field(){
     
     matrix[goal_y][goal_x].integration_field=0;
-    std::pair<int,int> arr[4] = {{-1,0},{1,0},{0,1},{0,-1}}; 
+    std::pair<int,int> arr[range_of_motion] = {{-1,0},{1,0},{0,1},{0,-1}}; 
 
     std::priority_queue<Node,std::vector<Node>,CompareCost> pq;
     pq.emplace(goal_x,goal_y,0);
@@ -49,7 +53,7 @@ void GenerateFlowField::Generate_Integration_Field(){
         auto current = pq.top();
         pq.pop();
 
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < range_of_motion; i++){
 
             int temp_x = current.x+arr[i].first;
             int temp_y = current.y+arr[i].second;
@@ -71,3 +75,86 @@ void GenerateFlowField::Generate_Integration_Field(){
     }
 
 }
+
+
+void GenerateFlowField::Generate_Flow_Field(){
+
+    std::pair<int,int> arr[range_of_motion] = {{-1,0},{1,0},{0,1},{0,-1}}; 
+
+
+    for(int x =0; x< tile_size_x; x++){
+        for(int y =0; y <tile_size_y; y++){
+            
+            float current_min = std::numeric_limits<float>::infinity();
+            std::pair<int,int> current_position = {-1,-1};
+
+            for(int i =0; i<range_of_motion; i++){
+                
+                int temp_x = x+arr[i].first;
+                int temp_y = y+arr[i].second;
+                
+
+            if((temp_x>=0 && temp_x<tile_size_x) && (temp_y>=0 && temp_y<tile_size_y)){
+                
+                auto value = matrix[temp_y][temp_x].integration_field;
+              if(current_min > value){
+
+                current_min = value;
+                current_position.first = temp_x;
+                current_position.second = temp_y;
+
+              }
+
+
+            }
+
+
+        }
+
+        matrix[y][x].next_Tile.first = current_position.first;
+        matrix[y][x].next_Tile.second = current_position.second;
+
+
+
+        }
+    }
+}
+
+
+void GenerateFlowField::Print_Cost_Field() const{
+
+    for(int x = 0; x<tile_size_x; x++){
+        for(int y =0; y < tile_size_y; y++){
+            std::cout << matrix[y][x].cost_field << " ";
+        }
+
+        std::cout << "\n";
+    }
+
+}
+
+void GenerateFlowField::Print_Integration_Field() const {
+
+    for(int x = 0; x<tile_size_x; x++){
+        for(int y =0; y < tile_size_y; y++){
+            std::cout << matrix[y][x].integration_field << " ";
+        }
+
+        std::cout << "\n";
+    }
+}
+
+
+
+void GenerateFlowField::Print_Flow_Field() const {
+
+    for(int x = 0; x<tile_size_x; x++){
+        for(int y =0; y < tile_size_y; y++){
+            std::cout << "(" << matrix[y][x].next_Tile.first << " , " << matrix[y][x].next_Tile.second << ")" << " ";
+        }
+
+        std::cout << "\n";
+    }
+}
+
+
